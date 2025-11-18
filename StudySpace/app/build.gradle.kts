c8813302da3d9496ae52a96bafcce975725d90ed
tuandepzai1506 <ntt150624@gmail.com>
2025-11-18 22:23:53 +0700

    no message

---
+++ StudySpace/app/build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.studyspace"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.example.studyspace"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    dependencies {
+
+        // CardView + RecyclerView
+        implementation("androidx.cardview:cardview:1.0.0")
+        implementation("androidx.recyclerview:recyclerview:1.3.2")
+
+        // Material Design Components
+        implementation("com.google.android.material:material:1.12.0")
+        implementation("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
+        implementation("com.google.android.material:material:1.13.0")
+    }
+
+
+}
---
+++ StudySpace/app/src/main/java/com/example/studyspace/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.studyspace;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.cardview.widget.CardView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import android.widget.TextView;
+import android.widget.ImageView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_main_act);
+
+
+        }
+
+
+
+
+}
+
