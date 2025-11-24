db48a44db333b8818ea986f667200b7e4addf472
tuandepzai1506 <ntt150624@gmail.com>
2025-11-24 17:10:01 +0700

    t push r nhe

---
+++ StudySpaceEdited/app/src/main/java/com/example/studyspace/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.studyspace;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.cardview.widget.CardView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.ImageView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_main_act);
+        ImageView add = (ImageView) findViewById(R.id.add);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    // 1. Tải và chạy Animation
+                    Animation flashAnim = AnimationUtils.loadAnimation(MainActivity.this, R.anim.button_press_animation);
+                    v.startAnimation(flashAnim);
+
+                    // 2. Chuyển Activity sau khi Animation kết thúc
+                    flashAnim.setAnimationListener(new Animation.AnimationListener() {
+                        @Override
+                        public void onAnimationStart(Animation animation) {}
+
+                        @Override
+                        public void onAnimationEnd(Animation animation) {
+                            Intent intent = new Intent();
+                            intent.setClass(getApplicationContext(), createQuizizz.class);
+                            startActivity(intent);
+                        }
+
+                        @Override
+                        public void onAnimationRepeat(Animation animation) {}
+                    });
+                };
+            });
+        };
+}
+
+
